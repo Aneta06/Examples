a94ee2ba53945719427584f9d422994f6eaf0501
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 12:56:32 +0200

    FceTwoVoid

---
+++ FceTwoVoid.java
@@ -0,0 +1,17 @@
+public class FceTwoVoid {
+
+    public static void main(String[] args) {
+
+        printNumber(10);                           // 2 . fce, slovo printNumber mam z fce vytiskne The number is 10
+    }
+
+    // void parameterized
+    public static void printNumber(int a) {              // 2. fce printNumber je nazev fce, kterej si vymyslim
+                                                        // ( pismeno a u int je nazev promenny)
+
+        System.out.println("The number is " + a);      //Vytiskne se pak The number is a clo ktery zadam nahore
+                                                      // do promenny print number
+
+
+    }
+}
