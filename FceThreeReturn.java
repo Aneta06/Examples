a9404ab6a1e8473958ad91e70b4ad4ae0e50d80b
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 12:57:05 +0200

    FceThreeReturn

---
+++ FceThreeReturn.java
@@ -0,0 +1,15 @@
+public class FceThreeReturn {
+
+    public static void main(String[] args) {
+
+        int result = giveMeTen();                     // 3 . fce, slovo result si vymyslim vytiskne 10
+
+        System.out.println(result);
+    }
+
+    // value-returning, parameterless
+    public static int giveMeTen() {                 // 3. fce giveMeTen je nazev fce, kterej si vymyslim
+
+        return 10;                                  // vrati se mi 10, kdyz to nahore vytisknu
+    }
+}
