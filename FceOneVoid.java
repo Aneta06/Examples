839206fdf0b04c2f8c524144efbfaf97b551449c
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 12:56:10 +0200

    FceOneVoid

---
+++ FceOneVoid.java
@@ -0,0 +1,14 @@
+public class FceOneVoid {
+
+
+    public static void main(String[] args) {
+
+
+        printHello();                          // 1. fce, slovo printHello mam z fce vytiskne Hello there!
+    }
+
+    //void, parameterlles method
+    public static void printHello() {         // 1. fce printHello je nazev fce kterej si vymyslim
+        System.out.println("Hello there!");    // Hello there, si vymyslim jakej chci mit vystup
+    }
+}
